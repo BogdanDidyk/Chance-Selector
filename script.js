d77c53dc82c65661017a152ad2e53fd623068bd8
Bogdan Didyk <bogdandidyk26@gmail.com>
2023-03-03 13:47:27 +0200

    Added funny example

--- script.js
+++ script.js
@@ -27,3 +27,18 @@ function simulateEvent(events, probabilities) {
     console.log("Your random event:", event);
     console.log("_".repeat(20), "\n");
 })();
+
+
+//Funny example
+(function() {
+    const rewards = [10, -25, 40, -35, -55, 105];
+    const probabilities = [0.3, 0.2, 0.2, 0.1, 0.15, 0.05];
+    let totalEarning = 0;
+    setInterval(() => {
+        const currentCasinoEarning = simulateEvent(rewards, probabilities);
+        totalEarning += currentCasinoEarning;
+        const text = (currentCasinoEarning < 0) ? "lose" : "won";
+        const style = (currentCasinoEarning < 0) ? "color:red;" : "color:green;";
+        console.log(`You %c${text}%c ${currentCasinoEarning}$, total = ${totalEarning}$`, style, "");
+    }, 1500);
+})();
